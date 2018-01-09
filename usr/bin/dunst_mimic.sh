5a18352f317e9c7f286d15ab226f92875cee2b22
Vitor Lopes <vlopes@gmail.com>
2018-01-09 12:29:26 +0000

    fix talking over

--- usr/bin/dunst_mimic.sh
+++ usr/bin/dunst_mimic.sh
@@ -3,13 +3,20 @@
 summary="$2"
 body="$3"
 
+# fix for pulse applet
 if [ "$summary" == "pa-applet" ]
 then    summary="Adjusting Output Volume";
 fi
 
-if pgrep -x "mimic" > /dev/null; 
-then     sleep 2s && mimic -t "$summary, $body" -voice slt && sleep 5s; 
-else     mimic -t "$summary, $body" -voice slt
-fi
-
-
+# avoid talking over
+count=0
+while :; do
+#    [ "$counter" -eq 6 ] && break
+    if pgrep -x "mimic" > /dev/null; then
+        (( counter += 1 ))
+        sleep 4s
+    else
+        mimic -t "$summary, $body" -voice slt
+        break
+    fi
+done
