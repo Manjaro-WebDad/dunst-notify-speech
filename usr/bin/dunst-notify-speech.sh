5a4c65a104b91df8887faf35662cc1b106746ea9
Vitor Lopes <vlopes@gmail.com>
2020-05-13 11:23:07 +0100

    rename naming to better represent its functionlaty

--- usr/bin/dunst-notify-speech.sh
+++ usr/bin/dunst-notify-speech.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Pass values to application from dunst. 
 # $2=notification-title, $3=notification-body
-/usr/share/webdad-speech/webdad-voice "$2" "$3"
+/usr/share/dunst-notify-speech/dunst-notify-mimic-voice "$2" "$3"
 
